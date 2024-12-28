3312039349339bceeff24f3197a7bb9a44467f9a
Akshat Goyal <goyalakshat111@gmail.com>
2024-12-28 16:56:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstWord(s) {
-  // your code here
+	let str = ''
+	for(let i=0;i<s.length;i++){
+		if((s[i]>='a'&&s[i]<='z')|| (s[i]>='A'&&s[i]<='Z')){
+			str +=s[i]	
+		}
+		else if(str.length>0){
+			break;
+		}
+	}
+	return str;
 }
 
 // Do not change the code below
